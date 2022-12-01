55109c44cf070d6f3388abab1d8ea77728c51967
niaogege <niaogege@users.noreply.github.com>
2022-12-01 08:38:36 +0000

    deploy: 3568ba686c58d98cd277f42c3593273ec8d282a1

---
+++ assets/work_goal.md.0d32dc33.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as i,o as t,d as l}from"./app.7bdec833.js";const f=JSON.parse('{"title":"\u76EE\u6807","description":"","frontmatter":{},"headers":[],"relativePath":"work/goal.md"}'),o={name:"work/goal.md"},a=l("",4),s=[a];function r(_,c,n,d,p,h){return t(),i("div",null,s)}const m=e(o,[["render",r]]);export{f as __pageData,m as default};
