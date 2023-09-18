b281fe58510db25f4f6d26d54dd890287a382b5d
niaogege <niaogege@users.noreply.github.com>
2023-09-18 12:01:29 +0000

    deploy: 1df1a0ef2d70bf2aec6793280e17c3db6b5fbbb3

--- assets/think_goal.md.4878183e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as l,o as i,c as t,Q as e}from"./chunks/framework.c8c775bf.js";const b=JSON.parse('{"title":"目标","description":"","frontmatter":{},"headers":[],"relativePath":"think/goal.md","filePath":"think/goal.md"}'),o={name:"think/goal.md"},s=e("",9),a=[s];function c(p,r,n,_,d,u){return i(),t("div",null,a)}const h=l(o,[["render",c]]);export{b as __pageData,h as default};
