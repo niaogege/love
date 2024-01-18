54286a96f31ded9cfc9a3d01549808f2574820b7
niaogege <niaogege@users.noreply.github.com>
2024-01-18 05:39:41 +0000

    deploy: a9c7f92343dae26b0a501d5878a92ce8b77b5a6f

---
+++ assets/think_cur.md.JV3wnCjB.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as l,V as t}from"./chunks/framework.MmnNiSBH.js";const f=JSON.parse('{"title":"Work","description":"","frontmatter":{},"headers":[],"relativePath":"think/cur.md","filePath":"think/cur.md"}'),r={name:"think/cur.md"},i=t("",10),o=[i];function s(n,h,c,u,d,_){return l(),a("div",null,o)}const b=e(r,[["render",s]]);export{f as __pageData,b as default};
---
+++ assets/think_goal.md.VtFLxjCl.lean.js
@@ -0,0 +1,1 @@
+import{_ as l,c as i,o as e,V as o}from"./chunks/framework.MmnNiSBH.js";const b=JSON.parse('{"title":"目标","description":"","frontmatter":{},"headers":[],"relativePath":"think/goal.md","filePath":"think/goal.md"}'),t={name:"think/goal.md"},a=o("",12),s=[a];function c(r,n,p,d,_,u){return e(),i("div",null,s)}const m=l(t,[["render",c]]);export{b as __pageData,m as default};
